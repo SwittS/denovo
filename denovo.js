1e5ff5ccd54a73b967cef01b022fc2678b830592
SwittS <switt.srikulacheep@gmail.com>
2017-02-16 14:22:22 -0600

    add additional conditional statements and show date table

--- denovo.js
+++ denovo.js
@@ -14,8 +14,7 @@ $('#scroll').click(function(){
 var table = $('mode-table').each(function() {
             });
 
- $('mode-table:not(#table_d0a6fb91d348)').addClass('hide');
-
+$('mode-table:not(#table_d0a6fb91d348, #table_ff996ca9a36d)').addClass('hide');
 $('funnel_1').addClass('hide');
 $('funnel_2').addClass('hide');
 
@@ -25,7 +24,7 @@ $(table).mouseenter(function() {
       var whichIndustry = $(this).attr('href');
       if (whichIndustry === "#Banking%20Services"){
         if(!mode-table.hasClass('banking-services')){
-           $('mode-table:not(#table_d0a6fb91d348)').addClass('hide');
+           $('mode-table:not(#table_d0a6fb91d348, #table_ff996ca9a36d)').addClass('hide');
         }
           $('.banking-services').removeClass('hide');
           $('html, body').animate({
@@ -33,20 +32,44 @@ $(table).mouseenter(function() {
         }, 1000);
       } else if (whichIndustry === "#Capital%20Markets"){
         if(!mode-table.hasClass('capital-markets')){
-           $('mode-table:not(#table_d0a6fb91d348)').addClass('hide');
+           $('mode-table:not(#table_d0a6fb91d348, #table_ff996ca9a36d)').addClass('hide');
         }
           $('.capital-markets').removeClass('hide');
           $('html, body').animate({
           scrollTop: $('.capital-markets').offset().top
         }, 1000);
       } else if (whichIndustry === "#Insurance"){
-        console.log('Insurance was clicked!');
+          if(!mode-table.hasClass('insurance')){
+             $('mode-table:not(#table_d0a6fb91d348, #table_ff996ca9a36d)').addClass('hide');
+          }
+            $('.insurance').removeClass('hide');
+            $('html, body').animate({
+            scrollTop: $('.insurance').offset().top
+          }, 1000);
       } else if (whichIndustry === "#Investment%20&%20Wealth%20Management"){
-        console.log('Investment & Wealth Management was clicked!');
+          if(!mode-table.hasClass('investment')){
+             $('mode-table:not(#table_d0a6fb91d348, #table_ff996ca9a36d)').addClass('hide');
+          }
+            $('.investment').removeClass('hide');
+            $('html, body').animate({
+            scrollTop: $('.investment').offset().top
+          }, 1000);
       } else if (whichIndustry === "#Technology%20Solutions"){
-        console.log('Technology Solutions was clicked!');
+          if(!mode-table.hasClass('technology')){
+             $('mode-table:not(#table_d0a6fb91d348, #table_ff996ca9a36d)').addClass('hide');
+          }
+            $('.technology').removeClass('hide');
+            $('html, body').animate({
+            scrollTop: $('.technology').offset().top
+          }, 1000);
       } else if (whichIndustry === "#Transaction%20&%20Payment%20Services"){
-        console.log('Transaction & Payment Services was clicked!');
+          if(!mode-table.hasClass('transaction')){
+             $('mode-table:not(#table_d0a6fb91d348, #table_ff996ca9a36d)').addClass('hide');
+          }
+            $('.transaction').removeClass('hide');
+            $('html, body').animate({
+            scrollTop: $('.transaction').offset().top
+          }, 1000);
       }
   });
 });
