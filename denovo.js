152bf4a66aebb3368767573e6dd654f985d15176
SwittS <switt.srikulacheep@gmail.com>
2017-02-16 15:59:03 -0600

    clean up code

--- denovo.js
+++ denovo.js
@@ -1,9 +1,9 @@
 $(window).scroll(function(){
-if ($(this).scrollTop() > 100) {
-  $('#scroll').fadeIn();
-} else {
-  $('#scroll').fadeOut();
-}
+  if ($(this).scrollTop() > 100) {
+    $('#scroll').fadeIn();
+  } else {
+    $('#scroll').fadeOut();
+  }
 });
 
 $('#scroll').click(function(){
@@ -11,8 +11,7 @@ $('#scroll').click(function(){
   return false;
 });
 
-var table = $('mode-table').each(function() {
-            });
+var table = $('mode-table').each(function(){});
 
 $('mode-table:not(#table_d0a6fb91d348, #table_ff996ca9a36d)').addClass('hide');
 $('funnel_1').addClass('hide');
@@ -24,7 +23,7 @@ $(table).mouseenter(function() {
       var whichIndustry = $(this).attr('href');
       if (whichIndustry === "#Banking%20Services"){
         if(!mode-table.hasClass('banking-services')){
-           $('mode-table:not(#table_d0a6fb91d348, #table_ff996ca9a36d)').addClass('hide');
+          $('mode-table:not(#table_d0a6fb91d348, #table_ff996ca9a36d)').addClass('hide');
         }
           $('.banking-services').removeClass('hide');
           $('html, body').animate({
@@ -32,7 +31,7 @@ $(table).mouseenter(function() {
         }, 1000);
       } else if (whichIndustry === "#Capital%20Markets"){
         if(!mode-table.hasClass('capital-markets')){
-           $('mode-table:not(#table_d0a6fb91d348, #table_ff996ca9a36d)').addClass('hide');
+          $('mode-table:not(#table_d0a6fb91d348, #table_ff996ca9a36d)').addClass('hide');
         }
           $('.capital-markets').removeClass('hide');
           $('html, body').animate({
@@ -40,7 +39,7 @@ $(table).mouseenter(function() {
         }, 1000);
       } else if (whichIndustry === "#Insurance"){
           if(!mode-table.hasClass('insurance')){
-             $('mode-table:not(#table_d0a6fb91d348, #table_ff996ca9a36d)').addClass('hide');
+            $('mode-table:not(#table_d0a6fb91d348, #table_ff996ca9a36d)').addClass('hide');
           }
             $('.insurance').removeClass('hide');
             $('html, body').animate({
@@ -48,7 +47,7 @@ $(table).mouseenter(function() {
           }, 1000);
       } else if (whichIndustry === "#Investment%20&%20Wealth%20Management"){
           if(!mode-table.hasClass('investment')){
-             $('mode-table:not(#table_d0a6fb91d348, #table_ff996ca9a36d)').addClass('hide');
+            $('mode-table:not(#table_d0a6fb91d348, #table_ff996ca9a36d)').addClass('hide');
           }
             $('.investment').removeClass('hide');
             $('html, body').animate({
@@ -56,7 +55,7 @@ $(table).mouseenter(function() {
           }, 1000);
       } else if (whichIndustry === "#Technology%20Solutions"){
           if(!mode-table.hasClass('technology')){
-             $('mode-table:not(#table_d0a6fb91d348, #table_ff996ca9a36d)').addClass('hide');
+            $('mode-table:not(#table_d0a6fb91d348, #table_ff996ca9a36d)').addClass('hide');
           }
             $('.technology').removeClass('hide');
             $('html, body').animate({
@@ -64,7 +63,7 @@ $(table).mouseenter(function() {
           }, 1000);
       } else if (whichIndustry === "#Transaction%20&%20Payment%20Services"){
           if(!mode-table.hasClass('transaction')){
-             $('mode-table:not(#table_d0a6fb91d348, #table_ff996ca9a36d)').addClass('hide');
+            $('mode-table:not(#table_d0a6fb91d348, #table_ff996ca9a36d)').addClass('hide');
           }
             $('.transaction').removeClass('hide');
             $('html, body').animate({
