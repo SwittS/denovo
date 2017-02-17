b63eb9654386a115b7dfa8f1f0ae36508bb54414
SwittS <switt.srikulacheep@gmail.com>
2017-02-17 14:58:32 -0600

    move show/hide table function to mode html

--- denovo.js
+++ denovo.js
@@ -11,28 +11,3 @@ $('#scroll').click(function(){
   $("html, body").animate({ scrollTop: 0}, 600);
   return false;
 });
-
-// hide all objects in dict
-$.each(dict, function(key, value){
-  $(value).addClass('hide');
-});
-
-// scroll to table and show function
-var table = $('mode-table').each(function(){});
-
-$(table).mouseenter(function(){
-  $('a').on("click", function(e){
-    e.preventDefault();
-    var whichIndustry = $(this).attr('href').toLowerCase().replace(/[^a-zA-Z0-9 ]+/g,"").replace(/\s+/g, "").replace(/\d+/g, '-');
-    $.each(dict, function(key, value){
-      if(whichIndustry !== key){
-        $(value).addClass('hide');
-      } else {
-        $(value).removeClass('hide');
-        $('html, body').animate({
-        scrollTop: $(value).offset().top
-        }, 1000);
-      }
-    });
-  });
-});
