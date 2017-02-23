00e0ab671291f30564f40fe097a70a50bcfa9bc1
SwittS <switt.srikulacheep@gmail.com>
2017-02-23 11:59:07 -0600

    consolidate last updated function to js and remove dead code from css

--- denovo.js
+++ denovo.js
@@ -11,3 +11,32 @@ $('#scroll').click(function(){
   $("html, body").animate({ scrollTop: 0}, 600);
   return false;
 });
+// end scroll to top function
+
+// start last updated function
+var bigText = [
+  {
+  "id": "date",
+  "column": "Info as of",
+  "label": "Info as of:",
+  "queryName": "DeNovo Date"
+  }
+];
+
+queries = [];
+
+datasets.forEach(function(d,i){ queries.push( [d.queryName,i] ); });
+
+$(".test").text(queries);
+
+try{
+  bigText.forEach(function(bn) {
+    var data = datasets.filter(function(d) { return d.queryName == bn.queryName; })[0].content;
+    var value = data[0][bn.column];
+    $("#" + bn.id).append("<p><span class='bn-label'>" + bn.label + "</span><br><br><span class='bn-number'>" + value + "</span></p>");
+  });
+}
+catch(err){
+  document.getElementById("date").innerHTML = err.message;
+}
+// end last updated function
